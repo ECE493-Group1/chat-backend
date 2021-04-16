2dd76616cf8cb4b08c3ae7c8359ff992733e02ce
Cody Gramlich <cmgramli@ualberta.ca>
2021-04-16 17:53:51 -0600

    Get KEYWORD_HOST environment variable in config

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"os"
+	"fmt"
+)
+
 type Config struct {
 	Port            string
 	CORSOrigin      string
@@ -8,10 +13,14 @@ type Config struct {
 }
 
 func GetConfig() *Config {
+	kw_host, exists := os.LookupEnv("KEYWORD_HOST")
+	if !exists {
+		kw_host = "localhost"
+	}
 	return &Config{
 		Port:            ":8000",
 		CORSOrigin:      "http://localhost:8080",
-		KeywordEndpoint: "http://localhost:5000/keywords/message",
+		KeywordEndpoint: fmt.Sprintf("http://%s:5000/keywords/message", kw_host),
 		KeywordUpdates:  true,
 	}
 }
